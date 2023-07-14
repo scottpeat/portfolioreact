5667814de9d61a145a38b3c59b0954e51eff9d04
Scott Peat <scottpeatweb@gmail.com>
2023-07-14 08:33:34 +0100

    Finished About section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import About from './components/about';
+import About from './components/About';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Testimonials from './components/Testimonials';
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">
+    <main className="text-gray-400 bg-gray-600 body-font">
       <Navbar />
       <About />
-      <Projects />
       <Skills />
-      <Testimonials />
+      <Projects />
       <Contact />
     </main>
   );
--- src/components/about.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const About = () => {
-  return <section id="about"></section>;
-};
-
-export default About;
